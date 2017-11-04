3c4acf751710cbef2d77cc22e879f970dbe09c85
sirricode <sirrikerimhakan@gmail.com>
2017-11-04 21:38:39 +0300

    App should work now.

--- app/src/main/java/com/sesayari/AudioManagerWidget.java
+++ app/src/main/java/com/sesayari/AudioManagerWidget.java
@@ -4,6 +4,7 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Button;
 import android.widget.RemoteViews;
 
@@ -18,8 +19,13 @@ public class AudioManagerWidget extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.audio_manager_widget);
-
-
+        Intent alNegIntent = new Intent(context, Ayar.class);
+        alNegIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        alNegIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetId);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                0, alNegIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.alarmNeg, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
--- app/src/main/java/com/sesayari/MainActivity.java
+++ app/src/main/java/com/sesayari/MainActivity.java
@@ -1,21 +1,147 @@
 package com.sesayari;
 
+import android.app.AlarmManager;
+import android.content.Context;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seekBars[] = new SeekBar[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SeekBar volumeSeekbar1 = (SeekBar)findViewById(R.id.seekBar2);
 
+        seekBars[0] = (SeekBar) findViewById(R.id.seekBar1);
+        seekBars[1] = (SeekBar) findViewById(R.id.seekBar2);
+        seekBars[2] = (SeekBar) findViewById(R.id.seekBar3);
+        seekBars[3] = (SeekBar) findViewById(R.id.seekBar4);
+
+        final AudioManager audioManager  = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        seekBars[0].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_ALARM));
+        seekBars[0].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_ALARM));
+        seekBars[1].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekBars[1].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_MUSIC));
+        seekBars[2].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL));
+        seekBars[2].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_VOICE_CALL));
+        seekBars[3].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_RING));
+        seekBars[3].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_RING));
+
+        seekBars[0].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
+            {
+                audioManager.setStreamVolume(AudioManager.STREAM_ALARM,
+                        progress, 0);
+            }
+        });
+        seekBars[1].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
+            {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                        progress, 0);
+            }
+        });
+        seekBars[2].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
+            {
+                audioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,
+                        progress, 0);
+            }
+        });
+        seekBars[3].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0)
+            {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2)
+            {
+                audioManager.setStreamVolume(AudioManager.STREAM_RING,
+                        progress, 0);
+            }
+        });
         Ayar sesAyari=new Ayar(this);
 
-        sesAyari.degistir(Ayar.Tip.alarm,Ayar.Yon.arti);
 
-
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final AudioManager audioManager  = (AudioManager) getSystemService(AUDIO_SERVICE);
+        seekBars[0] = (SeekBar) findViewById(R.id.seekBar1);
+        seekBars[1] = (SeekBar) findViewById(R.id.seekBar2);
+        seekBars[2] = (SeekBar) findViewById(R.id.seekBar3);
+        seekBars[3] = (SeekBar) findViewById(R.id.seekBar4);
+        seekBars[0].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_ALARM));
+        seekBars[0].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_ALARM));
+        seekBars[1].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekBars[1].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_MUSIC));
+        seekBars[2].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL));
+        seekBars[2].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_VOICE_CALL));
+        seekBars[3].setMax(audioManager
+                .getStreamMaxVolume(AudioManager.STREAM_RING));
+        seekBars[3].setProgress(audioManager
+                .getStreamVolume(AudioManager.STREAM_RING));
     }
 }
